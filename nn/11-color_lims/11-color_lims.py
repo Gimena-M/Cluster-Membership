36cc66d8b0da9c1d8255264854b19e407b4c70f1
unknown <gimenamartin144@gmail.com>
2023-06-12 18:13:57 -0300

    Test NN performance with magnitude and color limits

--- nn/11-color_lims/11-color_lims.py
+++ nn/11-color_lims/11-color_lims.py
@@ -10,17 +10,11 @@ from NNModelController import NNModelController
 data = DataHandler(validation_sample= True, features_txt= 'all_features.txt', fields_list=['W01', 'W02', 'W03', 'W04'], balance= 'weights')
 data.main()
 
-# Maximum values for i
-i_lims = [None, 21, 19]
-names = ['max_i_' + n for n in ['none','21','19']]
+# Test with no limits, with g-r or r-i smaller than 1., and with g-r or r-i larger than 1.
+gr_lim = 1.
+ri_lim = 1.
 
-# Train and test each model
-for i,nam in zip(i_lims, names):
-
-    if i:
-        data.feat_max = {"i_cmodel_mag": i}
-
-    # Architecture
+def func():
     layers = [
         tf.keras.layers.Dense(64, activation=tf.keras.activations.relu),
         tf.keras.layers.Dropout(0.2),
@@ -33,5 +27,38 @@ for i,nam in zip(i_lims, names):
         metrics=[]   
     )
     
-    mod = NNModelController(data = data.copy(), name = nam, layers = layers.copy(), compile_params= compile_params)
+    mod = NNModelController(data = data.copy(), name = name, layers = layers.copy(), compile_params= compile_params)
     mod.main(prep_data= True)
+
+# No limits
+data.feat_max = {}
+data.feat_min = {}
+name = "no_lim"
+func()
+
+# g - r >= 1.
+data.feat_max = {}
+data.feat_min = {"gr": gr_lim}
+name = "gr_larger_than_1"
+func()
+
+
+# g - r <= 1.
+data.feat_max = {"gr": gr_lim}
+data.feat_min = {}
+name = "gr_smaller_than_1"
+func()
+
+
+# r - i >= 1.
+data.feat_max = {}
+data.feat_min = {"ri": ri_lim}
+name = "ri_larger_than_1"
+func()
+
+
+# r - i <= 1.
+data.feat_max = {"ri": ri_lim}
+data.feat_min = {}
+name = "ri_smaller_than_1"
+func()
