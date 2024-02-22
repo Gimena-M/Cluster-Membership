4013cc936a8c74964bfacfecd599147e1a864751
unknown <gimenamartin144@gmail.com>
2024-02-22 17:46:53 -0300

    Re-run NN tests with sample divided by cluster redshift ranges

--- nn/6-z-steps/6-z-steps.py
+++ nn/6-z-steps/6-z-steps.py
@@ -1,5 +1,10 @@
+"""
+Test performance for clusters in z ranges with a 0.1 step, retrain and test again.
+"""
+
+import csv
 import sys
-import tensorflow as tf
+import numpy as np
 
 # Imports from Classes directory
 sys.path.append('../../Classes')
@@ -7,33 +12,53 @@ from DataHandler import DataHandler
 from NNModelController import NNModelController
 
 # Load and prepare data data 
-data = DataHandler(validation_sample= True, features_txt= 'all_features.txt', fields_list=['W01', 'W02', 'W03', 'W04'], balance= 'weights')
-data.main()
+data_ = DataHandler(validation_sample= True, features_txt= 'all_features.txt', balance= 'weights')
+data_.read_data()
 
 # Maximum values for z
-z_lims = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
+z_lims = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+features = ['all_features', 'all_features_bcg', 'all_features_sigmas']
+
+metrics_1 = []
+metrics_2 = []
 
 # Train and test each model
 for i in range(len(z_lims) - 1):
     
+    data = data_.copy()
     data.min_z = z_lims[i]
     data.max_z = z_lims[i + 1]
+    data.prep()
     name = f"z_{z_lims[i]}_to_{z_lims[i+1]}"
 
-    # Architecture
-    layers = [
-        tf.keras.layers.Dense(64, activation=tf.keras.activations.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(128, activation=tf.keras.activations.relu),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-        ]
-    compile_params = dict(
-        optimizer = tf.keras.optimizers.Adam(learning_rate= 0.0005),
-        loss = tf.keras.losses.BinaryCrossentropy(),
-        metrics=[]   
-    )
+    met1 = []
+    met2 = []
 
-    mod = NNModelController(data = data.copy(), name = name, layers = layers.copy(), compile_params= compile_params)
-    mod.main(prep_data= True)
+    for feat in features:
 
+        data.features_txt = feat + '.txt'
+        data.features_labels()
+
+        print(f'Testing {feat} {name}', flush = True)
+        mod = NNModelController(data = data.copy(), name = feat)
+        mod.main(model_exists= True, test_name= f'{feat}/{name}')
+        met1.append([mod.tester.p, mod.tester.r, mod.tester.specificity, mod.tester.accuracy, mod.tester.f1, mod.tester.pr_auc, mod.tester.roc_auc, mod.tester.threshold])
+
+        # Retrain
+        print(f'Training {feat} {name}', flush= True)
+        mod.main(model_exists= True, resume_training= True, test_name= f'{feat}/{name}--retrained', retrain_name= f'{feat}/{name}--retrained')
+        met2.append([mod.tester.p, mod.tester.r, mod.tester.specificity, mod.tester.accuracy, mod.tester.f1, mod.tester.pr_auc, mod.tester.roc_auc, mod.tester.threshold])
+
+    metrics_1.append(met1)
+    metrics_2.append(met2)
+
+metrics_1 = np.array(metrics_1)
+metrics_2 = np.array(metrics_2)
+
+with open('metrics.csv', 'w', newline= '') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Features', 'z_min', 'z_max', 'Precision', 'Recall', 'Specificity', 'Accuracy', 'F1-score', 'PR AUC', 'ROC AUC', 'Threshold', 'Precision (Ret)', 'Recall (Ret)', 'Specificity (Ret)', 'Accuracy (Ret)', 'F1-score (Ret)', 'PR AUC (Ret)', 'ROC AUC (Ret)', 'Threshold (Ret)'])
+
+    for i,feat in enumerate(features):
+        for i,(met1,met2) in enumerate(zip(metrics_1[:,i], metrics_2[:,i])):
+            writer.writerow([feat, z_lims[i], z_lims[i+1]]+['{:.4f}'.format(m) for m in met1]+['{:.4f}'.format(m) for m in met2])
