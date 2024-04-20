cbd3a8010709e0bd13a7e485ead035a1e833220b
unknown <gimenamartin144@gmail.com>
2024-04-20 13:34:42 -0300

    NN tests in local density bins

---
+++ nn/13-sigma_lims/13-sigma.py
@@ -0,0 +1,57 @@
+import csv
+import sys
+import numpy as np
+
+# Imports from Classes directory
+sys.path.append('../../Classes')
+from DataHandler import DataHandler
+from NNModelController import NNModelController
+
+data = DataHandler(validation_sample= True, features_txt= 'all_features.txt', balance= 'weights')
+data.read_data()
+
+feat_lims = dict(
+    sigma_7_10000 = [0, 2., 5., 10., 20., 50., 100., 200.],
+    sigma_10_5000 = [0, 1., 2., 5., 10., 20., 50., 100.],
+    sigma_10 = [5., 25., 50, 100., 200., 500., 1000., ],
+    sigma_5_10000 = [0, 2., 5., 10., 20., 50., 100., 200.],
+)
+
+features = ['all_features', 'all_features_bcg', 'all_features_sigmas']
+
+for fl in feat_lims.keys():
+
+    metrics_1 = []
+
+    lims = feat_lims[fl]
+    for i in range(len(lims) - 1):
+        
+        d = data.copy()
+        if lims[i+1]:
+            d.feat_max = {f"{fl}": lims[i+1]}
+        if lims[i]:
+            d.feat_min = {f"{fl}": lims[i]}
+        d.prep()
+
+        met1 = []
+
+        for feat in features:
+
+            d.features_txt = feat + '.txt'
+            d.features_labels()
+
+            mod = NNModelController(data = d, name = feat)
+            mod.main(model_exists= True, test_name= f'{fl}/{feat}/{lims[i]}_to_{lims[i+1]}', importances = [])
+            met1.append([mod.tester.p, mod.tester.r, mod.tester.specificity, mod.tester.accuracy, mod.tester.f1, mod.tester.pr_auc, mod.tester.roc_auc, mod.tester.threshold])
+
+        metrics_1.append(met1)
+
+    metrics_1 = np.array(metrics_1)
+
+    with open(f'metrics_{fl}.csv', 'w', newline= '') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Features', 'min', 'max', 'Precision', 'Recall', 'Specificity', 'Accuracy', 'F1-score', 'PR AUC', 'ROC AUC', 'Threshold'])
+
+        for i,feat in enumerate(features):
+            for i,met1 in enumerate(zip(metrics_1[:,i])):
+                writer.writerow([feat, lims[i], lims[i+1]]+['{:.4f}'.format(m) for m in met1[0]])
